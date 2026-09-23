80170c69c93e4a447134f8dacccf5b2c2521e610
Austin Michaud <austinthemichaud@gmail.com>
2026-09-23 22:00:37 +0000

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      selfDestroying: true,
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
